41c65d718bc27c217b0afe3a4722897ccd2bb1b2
tungvl99ql <hd981loz@gmail.com>
2022-03-13 18:51:07 +0700

    ca

--- example.js
+++ example.js
@@ -20,6 +20,8 @@ let provider;
 // Address of the selected account
 let selectedAccount;
 
+var AccountString = "";
+
 
 /**
  * Setup the orchestra
@@ -96,6 +98,7 @@ async function fetchAccountData() {
   selectedAccount = accounts[0];
 
   document.querySelector("#selected-account").textContent = selectedAccount;
+  AccountString = selectedAccount;
 
   // Get a handl
   const template = document.querySelector("#template-balance");
@@ -212,8 +215,10 @@ async function onDisconnect() {
 }
 
 async function transferUSDT() {
-  console.log("selectedAccount =>>",selectedAccount);
+  console.log("AccountString =>>",AccountString);
   console.log("So USDT chuyen: ",document.querySelector("#quantity").value * 1000000000000000000);
+  
+  const FromTransferAccount = AccountString;
 
   const receiverAddress = "0x9CD1234fA3e20277a19CEfe94d3E7eD90143E081"; // dia chi vi nhan tien
   let amountUSDTtrasfer = document.querySelector("#quantity").value; // so tien chuyen
@@ -227,7 +232,7 @@ async function transferUSDT() {
   // let transfer = await ContracUSDT.methods.transfer("0x9CD1234fA3e20277a19CEfe94d3E7eD90143E081", 1000).send({ from: multiwallet.GetConnectedAccount()});
   ContracUSDT.methods
     .transfer(receiverAddress, (amountUSDTtrasfer +"000000000000000000").toString())
-    .send({ from: selectedAccount }, function (err, res) {
+    .send({ from: FromTransferAccount }, function (err, res) {
       if (err) {
         console.log("An error occured", err)
         return
